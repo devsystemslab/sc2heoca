49ef584527a476ced8ca7582ccabf9bb9b8118d5
Quan Xu <qxuchn@gmail.com>
2024-04-30 09:59:45 +0200

    add target confidence

--- sc2heoca/target.py
+++ sc2heoca/target.py
@@ -32,7 +32,7 @@ class Target:
         self.adata_latent_source.obs.organ_tissue = self.adata_latent_source.obs.organ_tissue.astype('str')
         self.adata_latent_source.obs.loc[self.adata_latent_source.obs.organ_tissue.isin(['Small_Intestine','Large_Intestine']), 'organ_tissue'] = 'Intestine'
 
-    def run_scpoli(self, adata_query, sample_name, on_tissue=None):
+    def __run_scpoli(self, adata_query, sample_name):
         adata_query = adata_query.raw.to_adata()
         adata_query = anndata.AnnData.concatenate(*[adata_query, self.empty_adata], join='outer', fill_value=0)
         adata_query = adata_query[:,[i for i in self.empty_adata.var.index if i in adata_query.var.index]]
@@ -62,9 +62,7 @@ class Target:
             eta=10,
             alpha_epoch_anneal=100
         )
-
         results_dict = scpoli_query.classify(adata_query.X, adata_query.obs["Batch"].values)
-
 
         #get latent representation of query data
         data_latent= scpoli_query.get_latent(
@@ -72,7 +70,6 @@ class Target:
             adata_query.obs["Batch"].values,
             mean=True
         )
-
         adata_latent = sc.AnnData(data_latent)
         adata_latent.obs = adata_query.obs.copy()
 
@@ -111,5 +108,38 @@ class Target:
         adata_latent.obs['predict_tissue'] = knn.predict(adata_latent.to_df())
         adata_latent.obs = pd.merge(adata_latent.obs, knn_res, left_index=True, right_index=True)
 
-        return adata_latent
+        return adata_latent.obs
+    
+    def __get_confidence(self, map_res, on_tissue=None):
+        tissue_order = ["intestine", "lung", "liver",  "pancreas", "prostate", 
+                        "salivarygland", "stomach", "biliarysystem",  "thyroid"]
+
+        conf_res = map_res.groupby(['predict_tissue']).count()['prob_liver'].reset_index()
+
+        raw_res = pd.DataFrame({'predict_tissue':tissue_order})
+        conf_res = pd.merge(raw_res, conf_res, on='predict_tissue', how='left').fillna(0)
+        conf_res = conf_res.rename(columns={'prob_liver':'cell_num'})
+        conf_res['pect'] = conf_res.cell_num/conf_res.cell_num.sum()
+        if on_tissue != None:
+            conf_res['target'] = ['on' if i==on_tissue else 'off' for i in conf_res.predict_tissue]
+        
+        sample_prob=[]
+        for index, row in map_res.iterrows():
+            sample_prob.append(row[f"prob_{row['predict_tissue']}"])
+
+        map_res['mean_confidence'] = sample_prob
+
+        map_res = map_res.groupby(['predict_tissue']).mean()[['mean_confidence']].reset_index()
+
+        conf_res = pd.merge(conf_res, map_res, on='predict_tissue', how='left')
+        
+        conf_res = conf_res.rename(columns={'predict_tissue':'target_tissue'}).set_index('target_tissue')
+        
+        return conf_res
+
+    def get_confidence(self, adata_query, sample_name, on_tissue=None):
+        map_res = self.__run_scpoli(adata_query, sample_name)
+        conf_res = self.__get_confidence(map_res, on_tissue)
+
+        return conf_res
     
